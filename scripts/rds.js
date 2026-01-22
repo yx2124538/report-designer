a3af6c2380525a7b12c7893131fdfa39d11043ae
xinglie <kooboy_li@163.com>
2026-01-22 22:27:21 +0800

    2026-01-22

--- scripts/rds.js
+++ scripts/rds.js
@@ -1,11 +1,66 @@
 /**
- * report designer 打印、转换等服务
+ * report designer 打印服务
  */
-let puppeteer = require('puppeteer');
 let http = require('http');
-let NPrinter = require('node-printer');
-//window 系统安装打印机：https://github.com/tojocky/node-printer/issues/176
-//打印机自定义纸张使用mm单位，这里把其它单位转换为mm单位
+let puppeteer = require('puppeteer');
+let si = require('systeminformation');
+let { exec } = require('child_process');
+let fs = require('fs');
+//缓存服务
+let cache = new Map();
+let taskTail, expire = 30 * 60 * 1000;//30分钟过期
+let runTask = () => {
+    if (taskTail) {
+        let current = taskTail,
+            now = Date.now(),
+            prev;
+        while (current) {
+            if (now > current.expireTime) {
+                cache.delete(current.cacheId);
+                if (prev) {
+                    current = prev.taskPrev = current.taskPrev;
+                } else {
+                    current = taskTail = current.taskPrev;
+                }
+            } else {
+                prev = current;
+                current = current.prevTask;
+            }
+        }
+        setTimeout(runTask, 1000);
+    } else {
+        console.log('task empty');
+    }
+};
+let addTask = (cacheId, data) => {
+    if (!taskTail) {
+        setTimeout(runTask, 1000);
+    }
+    cache.set(cacheId, data);
+    taskTail = {
+        expireTime: Date.now() + expire,
+        cacheId,
+        prevTask: taskTail
+    };
+};
+let removeTask = cacheId => {
+    let current = taskTail,
+        prev;
+    while (current) {
+        if (current.cacheId == cacheId) {
+            cache.delete(cacheId);
+            if (prev) {
+                prev.taskPrev = current.taskPrev;
+            } else {
+                taskTail = current.taskPrev;
+            }
+            break;
+        }
+        prev = current;
+        current = current.prevTask;
+    }
+};
+//单位转换
 let mmToPXFactor = 1,
     pxToMMFactor = 1,
     remToPXFactor = 1,
@@ -20,23 +75,6 @@ let qToMM = n => n * 0.25;
 let mmToMM = n => n;
 let remToMM = n => n * remToPXFactor * pxToMMFactor;
 let rlhToMM = n => n * rlhToPxFactor * pxToMMFactor;
-
-let PAGE_BASIC_READY = 2,
-    PAGE_FULL_RENDER = 4,
-    PAGE_RECEIVE_SERVER_DATA = 8;
-
-let unitsConverter = {
-    px: pxToMM,
-    pt: ptToMM,
-    in: inToMM,
-    cm: cmToMM,
-    q: qToMM,
-    pc: pcToMM,
-    mm: mmToMM,
-    rem: remToMM,
-    rlh: rlhToMM
-};
-let delay = timespan => new Promise(resolve => setTimeout(resolve, timespan));
 let updateFactor = async page => {
     if (mmToPXFactor == 1) {//未处理过
         [measureWidth, remToPXFactor, rlhToPxFactor] = await Promise.all([page.evaluate(() => {
@@ -61,227 +99,180 @@ let updateFactor = async page => {
         pxToMMFactor = 1 / mmToPXFactor;
     }
 };
-//以下是简易缓存，请替换为更专业的缓存处理
-let cache = new Map();
-let maxExistPages = 10;
-let newPageCount = 0;
-let setCachePage = (page, destURL) => {
-    if (newPageCount < maxExistPages) {
-        let promise = page.goto(destURL, {
-            waitUntil: 'domcontentloaded',
-            timeout: 10000
-        });
-        let c = {
-            promise,
-            page
-        };
-        if (cache.has(destURL)) {
-            let set = cache.get(destURL);
-            set.push(c);
-        } else {
-            cache.set(destURL, [c]);
-        }
-    } else {
-        page.close();
-        newPageCount--;
-    }
+let unitsConverter = {
+    px: pxToMM,
+    pt: ptToMM,
+    in: inToMM,
+    cm: cmToMM,
+    q: qToMM,
+    pc: pcToMM,
+    mm: mmToMM,
+    rem: remToMM,
+    rlh: rlhToMM
 };
-let getCachePage = async (browser, destURL) => {
-    if (cache.has(destURL)) {
-        let set = cache.get(destURL);
-        if (set.length) {
-            let c = set.pop();
-            console.log('use cache page');
-            return [c.page, c.promise];
-        }
-    }
-    console.log('create new page');
-    newPageCount++;
-    let page = await browser.newPage();
-    await updateFactor(page);
-    let p = page.goto(destURL, {
-        waitUntil: 'domcontentloaded',
-        timeout: 10000
-    });
-    return [page, p];
-};
-(async () => {
-    let browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: null,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        ignoreHTTPSErrors: true
-    });
-    let rId = 0;
-    http.createServer((request, response) => {
-        let data = [];
-        request.on('data', chunk => {
-            data.push(chunk);
+
+let getPayload = async (request) => {
+    return new Promise((resolve, reject) => {
+        let payload = '';
+        request.on('data', (chunk) => {
+            payload += chunk.toString();
         });
-        request.on('end', async () => {
+        request.on('end', () => {
             try {
-                let { headers, url, method } = request;
-                let baseURL = 'http://' + headers.host;
-                let enter = new URL(url, baseURL);
-                let ref = headers.origin || headers.referer;
-                response.setHeader('Access-Control-Allow-Origin', ref);
-                response.setHeader('Access-Control-Allow-Credentials', 'true');
-                response.setHeader('Access-Control-Allow-Headers', 'X-Type,Auth');
-                if (method == 'OPTIONS') {
-                    response.end();
-                    return;
-                }
-                let srcData = data.join('');
-                if (!srcData.startsWith('data=')) {
-                    console.info('unexpect params or invoked ', srcData);
-                    response.write(JSON.stringify({
-                        success: false,
-                        message: `unexpect params or invoked`
-                    }));
-                    response.end();
-                    return;
-                }
-                let urlParams = new URLSearchParams(srcData);
-                let json = JSON.parse(urlParams.get('data'));
-                let u = new URL(json.location);
-                u.searchParams.set(json.key, PAGE_BASIC_READY);
-                let returned = {
-                    success: true,
-                    data: []
-                };
-                //获取打印机列表
-                if (enter.pathname == '/printers') {
-                    let list = NPrinter.list();
-                    returned.data.push(...list);
+                resolve(JSON.parse(payload));
+            } catch (err) {
+                reject(new Error(`解析 payload 失败：${err.message}`));
+            }
+        });
+        request.on('error', (err) => {
+            reject(new Error(`读取请求数据失败：${err.message}`));
+        });
+    });
+};
+let wrapResult = (success, message = '', data = null) => {
+    return JSON.stringify({
+        success,
+        data,
+        message
+    });
+};
+let wrapPage = (basic, pages) => `<!DOCTYPE html><html><head><meta charset="utf-8" />${basic.styles.join('')}</head><body>${basic.symbols}${pages.join('')}</body></html>`;
+
+
+let toPrinter = (printerName, file) => {
+    let platform = process.platform;
+    return new Promise((resolve, reject) => {
+        if (platform === 'win32') {
+            //window平台安装SumatraPDF.exe
+            //https://www.sumatrapdfreader.org/free-pdf-reader
+            //下面的路径需要修改为您安装的目录
+            exec(`C:\\your path to\\SumatraPDF.exe -print-to "${printerName}" -silent "${file}"`, error => {
+                if (error) {
+                    reject(error);
                 } else {
-                    let requestId = 'request-' + rId++;
-                    let destURL = u.toString();
-                    let [page, gotoPromise] = await getCachePage(browser, destURL);
-                    console.info(`[info]${requestId} render page:`, destURL);
-                    let { stage } = json;
-                    let { page: rdPage, unit } = stage;
-                    console.info(`[info]${requestId} page unit:`, unit);
-                    let convert = unitsConverter[unit];
+                    resolve();
+                }
+            });
+        } else if (platform === 'darwin') {
+            exec(`lp -d ${printerName} ${file}`, error => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
+            });
+        } else {
+            reject(new Error(`Unsupported platform: ${platform}`));
+        }
+    });
+};
+let startId = Date.now();
+let currentId = 0;
+
+puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--kiosk-printing'],
+    ignoreHTTPSErrors: true
+}).then(browser => {
+    http.createServer(async (request, response) => {
+        let { headers, method } = request;
+        let ref = headers.origin || headers.referer || 'https://demo.report-designer.cn';
+        response.setHeader('Content-Type', 'application/json; charset=utf-8');
+        response.setHeader('Access-Control-Allow-Origin', ref);
+        response.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+        response.setHeader('Access-Control-Allow-Credentials', 'true');
+        response.setHeader('Access-Control-Allow-Headers', 'X-Type, Auth, Content-Type, X-Requested-With');
+        if (method == 'OPTIONS') {
+            response.writeHead(204);
+            response.end();
+            return;
+        }
+        try {
+            let data = await getPayload(request);
+            if (data.flag == 1) {//获取打印机
+                let printers = await si.printer();
+                let printerNames = [];
+                for (let p of printers) {
+                    printerNames.push(p.name);
+                }
+                response.end(wrapResult(true, '', printerNames));
+            } else if (data.flag == 2) {//基础数据
+                addTask(data.taskId, {//需要考虑客户端发送数据异常，服务端不长久持有数据
+                    symbols: data.symbols,
+                    printer: data.printer,
+                    styles: data.styles
+                });
+                response.end(wrapResult(true));
+            } else if (data.flag == 4) {//取消
+                removeTask(data.taskId);
+                response.end(wrapResult(true));
+            } else if (data.flag == 3) {//分批打印
+                let basic = cache.get(data.taskId);
+                if (basic) {
+                    let convert = unitsConverter[data.unit];
                     if (!convert) {
-                        throw new Error('can not process unit:' + unit);
+                        response.end(wrapResult(false, 'can not process unit:' + data.unit));
+                        return;
                     }
-                    //一些像派卡尺寸，在生成pdf时不支持，这里统一转为毫米尺寸
-                    let mmWidth = convert(rdPage.width),
-                        mmHeight = convert(rdPage.height);
-                    //视图需要px单位，这里转换为px
+                    let page = await browser.newPage();
+                    await updateFactor(page);
+                    let mmWidth = convert(data.width),
+                        mmHeight = convert(data.height);
                     let pxWidth = (mmToPX(mmWidth) + 1) | 0,
                         pxHeight = (mmToPX(mmHeight) + 2) | 0;
-                    console.info(`[info]${requestId} action:`, enter.pathname);
-                    console.info(`[info]${requestId} size`, pxWidth, pxHeight);
-                    await Promise.all([
-                        page.setViewport({
-                            //设置可视区域是单个页面区域+50,防止把滚动条截入图里
-                            width: pxWidth + 50,
-                            height: pxHeight + 50,
-                            deviceScaleFactor: 3
-                        }),
-                        gotoPromise,
-                        page.emulateMediaType('print')
-                    ]);
-                    let start = Date.now(),
-                        isTimeout = false,
-                        currentState,
-                        timeout = 3 * 60 * 1000;//3分钟超时
-                    while (true) {
-                        let state = await page.evaluate(() => document.rdState);
-                        console.info(`[info]${requestId} receive page state`, state);
-                        if (state & PAGE_FULL_RENDER) {//页面渲染完成
-                            break;
-                        } else if ((state & PAGE_BASIC_READY) &&//页面基础完成
-                            !(state & PAGE_RECEIVE_SERVER_DATA)) {//尚未收到数据
-                            await page.evaluate(s => {
-                                let e = new Event('render');
-                                e.json = JSON.stringify(s);
-                                window.dispatchEvent(e);
-                            }, stage);
-                        } else if (Date.now() - start > timeout) {
-                            currentState = state;
-                            isTimeout = true;
-                            break;
-                        }
-                        await delay(150);
+                    let basicInfo = basic.printer;
+                    let nOptions = {
+                        'page-bottom': 0,
+                        'page-left': 0,
+                        'page-right': 0,
+                        'page-top': 0,
+                        //n: basicInfo.nc,
+                        media: `Custom.${mmWidth}x${mmHeight}mm`,
+                    }, ts = basicInfo.ts,
+                        pName = basicInfo.name;
+                    if (ts) {
+                        nOptions.sides = `two-sided-${mmHeight > mmWidth ? 'short' : 'long'}-edge`;
                     }
-                    if (isTimeout) {
-                        throw new Error('timeout,state is:' + currentState);
-                    } else {
-                        console.info(`[info]${requestId} page ready`);
-                    }
-                    //生成pdf
-                    if (enter.pathname == '/pdf') {
-                        //如果要设置pdf meta需要再引入其它库：https://stackoverflow.com/questions/51153026/puppeteer-pdf-title-and-author-metadata
-                        let pdf = await page.pdf({
+                    await page.setViewport({
+                        //设置可视区域是单个页面区域+50,防止把滚动条截入图里
+                        width: pxWidth + 50,
+                        height: pxHeight + 50,
+                        deviceScaleFactor: 3
+                    });
+                    await page.emulateMediaType('print');
+                    await page.setContent(wrapPage(basic, data.pages), { waitUntil: 'networkidle0' });
+                    if (cache.get(data.taskId)) {//需要考虑在chrome准备页面的过程中，客户端取消请求后，未进入打印队列的直接取消
+                        let file = `./${startId}_${currentId++}.pdf`;
+                        await page.pdf({
+                            path: file,
                             printBackground: true,
                             width: pxWidth + 'px',
                             height: pxHeight + 'px'
                         });
-                        returned.data.push(pdf.toString('base64'));
-                        //生成图片
-                    } else if (enter.pathname == '/image') {
-                        let pages = await page.$$('[role="page-content"]');
-                        console.info(`[info]${requestId} find pages`, pages.length);
-                        let index = 0;
-                        for (let page of pages) {
-                            let img = await page.screenshot({
-                                encoding: "base64"
-                            });
-                            console.info(`[info]${requestId} convert image`, index++);
-                            returned.data.push('data:image/png;base64,' + img);
+                        //使用浏览器自身的打印
+                        //await page.evaluate(() => window.print());
+                        //指定打印机打印
+                        //await toPrinter(pName, file);
+                        //清理文件
+                        //fs.unlinkSync(file);
+                        if (data.complete) {
+                            removeTask(data.taskId);
                         }
-                    } else if (enter.pathname == '/print') {
-                        //执行打印
-                        let {
-                            nc, name: pName, l: landscape, ts
-                        } = json.printer;
-                        let nOptions = {
-                            'page-bottom': 0,
-                            'page-left': 0,
-                            'page-right': 0,
-                            'page-top': 0,
-                            n: nc,
-                            media: `Custom.${mmWidth}x${mmHeight}mm`,
-                            fitplot: true
-                        };
-                        if (!pName) {
-                            let list = NPrinter.list();
-                            pName = list[0];
-                        }
-                        let l = 0;
-                        if (landscape == 'landscape') {
-                            nOptions.landscape = 'landscape';
-                            l = 1;
-                        }
-                        if (ts) {
-                            nOptions.sides = `two-sided-${l ? 'short' : 'long'}-edge`;
-                        }
-                        let printer = new NPrinter(pName);
-                        let pages = await page.$$('[role="page-content"]');
-                        for (let page of pages) {
-                            let img = await page.screenshot();
-                            printer.printBuffer(img, nOptions);
-                        }
-                    } else {//失败返回提示信息
-                        returned.success = false;
-                        returned.message = 'unsupport ' + request.url;
+                    } else {
+                        console.log('cancelled');
                     }
-                    setCachePage(page, destURL);
-                    console.info(`[info]${requestId} returned`);
+                    await page.close();
+                    response.end(wrapResult(true));
+                } else {//不存在的任务
+                    response.end(wrapResult(false, 'can not find:' + data.taskId));
                 }
-                response.write(JSON.stringify(returned));
-                response.end();
-            } catch (ex) {
-                console.error('exception:', ex);
-                response.write(JSON.stringify({
-                    success: false,
-                    message: ex.message
-                }));
-                response.end();
+            } else {//未能识别参数
+                response.end(wrapResult(false, 'unknown parameters'));
             }
-        });
+        } catch (ex) {
+            response.end(wrapResult(false, ex.message));
+        }
     }).listen(9898);
     console.log('report designer server ready~');
-})();
+});
